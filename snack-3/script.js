a183232c371234d68a3297eb0dcc70a79f203bfc
TommasoVilla91 <tommasovilla@yahoo.it>
2024-11-15 15:09:31 +0100

    feat: snack 3 completed

--- snack-3/script.js
+++ snack-3/script.js
@@ -2,3 +2,10 @@ const numbers = [2, 8, 4, 7, 2, 87];
 
 // Crea un nuovo array in cui tutti i numeri siano incrementati di 1
 // Risultato: [3, 9, 5, 8, 3, 88]
+
+const plusOneNums = numbers.map((num) => {
+    num++;
+    return num;
+});
+
+console.log(plusOneNums);
